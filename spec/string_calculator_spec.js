6b88937d2f59540a52dff81d14d91f5ead6a4d8d
Nikola Cvetkovic <nikolacvetkovic.6969@gmail.com>
2025-06-19 15:47:05 +0200

    more test got added they all pass

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -18,5 +18,17 @@ describe('String Calculator', function() {
 		expect(calculator.add('1,27')).toEqual(28);
 	});
 
-	
+	it('should return the sum of three numbers', function() {
+		expect(calculator.add('15,17,21')).toEqual(53);
+	});
+
+	it('should return the sum of multiple numbers', function() {
+		expect(calculator.add('1,5,27,45')).toEqual(78);
+		expect(calculator.add('1,10,100,1000,10000')).toEqual(11111);
+	});
+
+	it('should ignore non-numeric values', function() {
+		expect(calculator.add('A,10')).toEqual(10);
+		expect(calculator.add('abc,5,xyz,15')).toEqual(20);
+	});
 });
