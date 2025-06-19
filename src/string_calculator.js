ed64c9cdf512297d6b1de02a045a1ee5a51af5f6
Nikola Cvetkovic <nikolacvetkovic.6969@gmail.com>
2025-06-19 15:44:35 +0200

    src code got added first test passes now

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,4 +1,11 @@
 // StringCalculator.js
 function StringCalculator() {}
+StringCalculator.prototype.add = function(string_numbers) {
+	if (!string_numbers) return 0;
 
-
+	return string_numbers
+		.split(',')                           // Teile an Kommas
+		.map(item => parseInt(item, 10))      // Versuche jede Zahl zu parsen
+		.filter(num => !isNaN(num))           // Ignoriere nicht-numerische Werte wie "A"
+		.reduce((sum, num) => sum + num, 0);  
+};
